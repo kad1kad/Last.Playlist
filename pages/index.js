b318dba9f6257dc664ec3108b0f73062df20cf5f
kad1kad <k.aliu@me.com>
2022-07-05 19:11:21 +0100

    add logindisplay and last.fm user selector

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import UserSelector from "../components/UserSelector";
+import LoginDisplay from "../components/LoginDisplay";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    <div>
+      <LoginDisplay />
+      <UserSelector />
+    </div>
   );
 }
