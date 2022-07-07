eb5c350df5a63660dbfa77d952d7640ef0a4f492
kad1kad <k.aliu@me.com>
2022-07-07 10:23:15 +0100

    add header

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
 import UserSelector from "../components/UserSelector";
 import LoginDisplay from "../components/LoginDisplay";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-black min-h-screen w-full text-slate-50">
-      <LoginDisplay />
+    <div className="bg-black w-full text-slate-50 min-h-[100vh] pt-5">
+      <header className="px-4 flex justify-between items-center">
+        <div>
+          <Link href="/">
+            <a>
+              <h1 className="text-lg tracking-widest font-bold text-blue-300">
+                Last.Playlist
+              </h1>
+            </a>
+          </Link>
+          <h2 className="text-sm font-light tracking-wider">
+            Last.fm to Spotify playlist
+          </h2>
+        </div>
+
+        <LoginDisplay />
+      </header>
+
       <UserSelector />
     </div>
   );
