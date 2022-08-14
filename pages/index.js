9a4e9afe3e4976cdba2f8d0e6c77170c5e9b61d5
kad1kad <k.aliu@me.com>
2022-08-14 23:36:42 +0100

    style: bg color change

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,18 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-black w-full text-slate-50 min-h-[100vh] pt-5">
+    <div className="bg-[#101113] w-full text-slate-50 min-h-[100vh] pt-5">
       <header className="px-4 flex justify-between items-center">
         <div>
           <Link href="/">
             <a>
-              <h1 className="text-lg tracking-widest font-bold text-blue-300">
+              <h1 className="text-2xl tracking-widest font-bold text-blue-500">
                 Last.Playlist
               </h1>
             </a>
           </Link>
           <h2 className="text-sm font-light tracking-wider">
-            Last.fm to Spotify playlist
+            Last.fm to Spotify Playlist
           </h2>
         </div>
 
