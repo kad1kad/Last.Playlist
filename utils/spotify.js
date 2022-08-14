a9dd47e1155cbd6dbf5a89458a0c80e61230413f
kad1kad <k.aliu@me.com>
2022-08-14 15:28:31 +0100

    change spotify scopes

--- utils/spotify.js
+++ utils/spotify.js
@@ -3,21 +3,19 @@ import SpotifyWebApi from "spotify-web-api-node";
 const scopes = [
   "user-read-email",
   "playlist-read-private",
-  "playlist-read-collaborative",
+  "playlist-modify-private",
   "streaming",
   "user-read-private",
   "user-library-read",
   "user-library-modify",
-  "user-top-read",
   "user-read-playback-state",
   "user-modify-playback-state",
   "user-read-currently-playing",
   "user-read-recently-played",
-  "user-follow-read",
 ].join(",");
 
 const params = {
-  scrope: scopes,
+  scope: scopes,
 };
 
 const queryParamString = new URLSearchParams(params);
