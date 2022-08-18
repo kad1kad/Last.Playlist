0869839d1302cb46fc1633adcfe5ef2cad8fd26c
kad1kad <k.aliu@me.com>
2022-08-18 13:27:42 +0100

    create as separate comp

---
+++ components/PeriodSelector.js
@@ -0,0 +1,18 @@
+import periods from "../utils/periods";
+
+function PeriodSelector({ handlePeriod }) {
+  return (
+    <div className="flex items-center justify-center mt-4 gap-3">
+      <p>Top Tracks period:</p>
+      <select onChange={handlePeriod} className="text-slate-900 rounded-md ">
+        {periods.map((period, index) => (
+          <option key={index} value={period}>
+            {period}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default PeriodSelector;
