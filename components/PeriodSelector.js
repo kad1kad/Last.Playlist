544b413cb9e27bab047f5873ee4d825ee9609b1c
kad1kad <k.aliu@me.com>
2022-08-18 14:43:43 +0100

    remove scroll smooth

--- components/PeriodSelector.js
+++ components/PeriodSelector.js
@@ -2,9 +2,9 @@ import periods from "../utils/periods";
 
 function PeriodSelector({ handlePeriod }) {
   return (
-    <div className="flex items-center justify-center mt-4 gap-3">
+    <div className="flex items-center justify-center mt-4 gap-3 scroll-smooth">
       <p>Top Tracks period:</p>
-      <select onChange={handlePeriod} className="text-slate-900 rounded-md ">
+      <select onChange={handlePeriod} className="text-slate-900 rounded-md">
         {periods.map((period, index) => (
           <option key={index} value={period}>
             {period}
