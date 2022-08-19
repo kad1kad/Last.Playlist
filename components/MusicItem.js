225d8be4de60e162c87bed04b825301f5bee8513
kad1kad <k.aliu@me.com>
2022-08-19 14:07:40 +0100

    add framer.motion

--- components/MusicItem.js
+++ components/MusicItem.js
@@ -1,17 +1,25 @@
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 function MusicItem({ item }) {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      exit={{ delay: 2, opacity: 0 }}
+      key={item.name}
+      src={item.name}
+    >
       <Link href={item.url}>
         <a>
-          <li className="leading-8 text-center transition-all ease-in-out hover:scale-105">
+          <li className="leading-8 text-center">
             <h3 className="inline "> {item.artist.name} – </h3>
             <p className="inline">{item.name} </p>
           </li>
         </a>
       </Link>
-    </>
+    </motion.div>
   );
 }
 
--- components/MusicList.js
+++ components/MusicList.js
@@ -1,14 +1,17 @@
 import MusicItem from "./MusicItem";
+import { AnimatePresence, motion } from "framer-motion";
 
 function MusicList({ musicItems }) {
   console.log(musicItems);
   return (
     <section className="my-5">
-      <ul>
-        {musicItems.toptracks?.track.map((item, index) => (
-          <MusicItem key={index} item={item} />
-        ))}
-      </ul>
+      <AnimatePresence>
+        <ul>
+          {musicItems.toptracks?.track.map((item, index) => (
+            <MusicItem key={index} item={item} />
+          ))}
+        </ul>
+      </AnimatePresence>
     </section>
   );
 }
